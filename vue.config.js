45ad28988e24801ab4e3e5cc08dfe25278ad173d
Sunny Tseng <tzuchientseng@gmail.com>
2025-01-24 15:54:07 +0800

    Version 1.4

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'https://home.sunnytseng.com/api',
-        changeOrigin: true, // 是否允許跨域
-        pathRewrite: { '^/api': '' }, // 將 /api 替換為空（視需求設定）
-      },
-    },
-  },
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://home.sunnytseng.com/api',
+  //       changeOrigin: true, // 是否允許跨域
+  //       pathRewrite: { '^/api': '' }, // 將 /api 替換為空（視需求設定）
+  //     },
+  //   },
+  // },
 });
 
 /*
